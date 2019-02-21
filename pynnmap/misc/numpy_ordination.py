cf4e9b9f5113bcee11f445d726107905f2d546c9
Matt Gregory <matt.gregory@oregonstate.edu>
2019-02-21 15:55:49 -0800

    Rename matrix variables to conform with PEP8

--- pynnmap/misc/numpy_ordination.py
+++ pynnmap/misc/numpy_ordination.py
@@ -2,6 +2,8 @@ import math
 
 import numpy as np
 
+ZERO = 0.0001
+
 
 class NumpyCCA(object):
     """
@@ -23,32 +25,25 @@ class NumpyCCA(object):
 
         Parameters
         ----------
-        X : numpy array
+        x : np.array
             Species matrix
-
-        Y : numpy array
+        y : np.array
             Environment matrix
-
-        Returns
-        -------
-        None
         """
-        self.X = X
-        self.Y = Y
-
-        ZERO = 0.0001
+        self.x = x
+        self.y = y
 
         # Standardize the species matrix
-        X = self.X / np.sum(self.X)
+        x = self.x / np.sum(self.x)
 
         # Ensure that all species rows (plots) have values
-        row_sums = np.sum(X, axis=1)
+        row_sums = np.sum(x, axis=1)
         if np.any(row_sums) <= 0.0:
             err_str = 'There were plots with no tally'
             raise ValueError(err_str)
 
         # Ensure that all species columns (species) have values
-        col_sums = np.sum(X, axis=0)
+        col_sums = np.sum(x, axis=0)
         if np.any(col_sums) <= 0.0:
             err_str = 'There were species with no tally'
             raise ValueError(err_str)
@@ -57,19 +52,19 @@ class NumpyCCA(object):
         rc = np.outer(row_sums, col_sums)
 
         # Chi-square contributions
-        X_bar = (X - rc) / np.sqrt(rc)
+        x_bar = (x - rc) / np.sqrt(rc)
 
         # Standardize the environmental matrix based on species row_sums
-        (self.Y_r, weighted_means) = self._weight_center(self.Y, row_sums)
+        self.y_r, weighted_means = self._weight_center(self.y, row_sums)
 
-        # Perform QR decompostion on the weighted Y matrix
-        (self.Q, self.R) = np.linalg.qr(self.Y_r, mode='full')
+        # Perform QR decomposition on the weighted Y matrix
+        self.q, self.r = np.linalg.qr(self.y_r, mode='full')
 
         # Run MLR fitting for Y matrix
-        right = np.dot(self.Q.T, X_bar)
-        (ls, res, rank, s) = np.linalg.lstsq(self.R, right)
-        Y = np.dot(self.Y_r, ls)
-        (self.u_raw, s, v) = np.linalg.svd(Y, full_matrices=False)
+        right = np.dot(self.q.T, x_bar)
+        ls, res, rank, s = np.linalg.lstsq(self.r, right)
+        y = np.dot(self.y_r, ls)
+        self.u_raw, s, v = np.linalg.svd(y, full_matrices=False)
 
         # Transform v for later calculations
         v = v.T
@@ -93,7 +88,7 @@ class NumpyCCA(object):
         self.u_eig = np.multiply(self.u, s[0:rank])
         self.v_eig = np.multiply(self.v, s[0:rank])
 
-        a = np.dot(X_bar, v[:, 0:rank])
+        a = np.dot(x_bar, v[:, 0:rank])
         self.wa_eig = np.multiply(a, u_weight)
 
         self.wa = np.multiply(self.wa_eig, (1.0 / s[0:rank]))
@@ -102,22 +97,22 @@ class NumpyCCA(object):
         w_c = np.average(x, axis=0, weights=w)
         x = x - w_c
         x = np.apply_along_axis(np.multiply, 0, x, np.sqrt(w))
-        return (x, w_c)
+        return x, w_c
 
     def biplot_scores(self):
         """
         Return biplot scores of environmental variable by axis
         """
         biplot_scores = np.corrcoef(
-            self.Y_r, self.u_raw[:, 0:self.rank], rowvar=0)
+            self.y_r, self.u_raw[:, 0:self.rank], rowvar=False)
         return biplot_scores[0:self.rank, self.rank:]
 
     def coefficients(self):
         """
         Return the environmental variable loadings of for each axis
         """
-        right = np.dot(self.Q.T, self.u_raw[:, 0:self.rank])
-        (x, resid, rank, s) = np.linalg.lstsq(self.R, right)
+        right = np.dot(self.q.T, self.u_raw[:, 0:self.rank])
+        (x, residual, rank, s) = np.linalg.lstsq(self.r, right)
         return np.array(x)
 
     def species_centroids(self):
@@ -137,7 +132,7 @@ class NumpyCCA(object):
             (uk.shape[0], xi.shape[0], xi.shape[1]), dtype=np.float64)
         for (i, s) in enumerate(uk):
             xiuk[i] = xi - s
-        y = self.X.T
+        y = self.x.T
         y_xiuk_sqr = np.zeros((uk.shape[0], uk.shape[1]), dtype=np.float64)
         for i in range(y.shape[0]):
             y_xiuk_sqr[i] = np.dot(y[i], np.square(xiuk[i]))
@@ -146,12 +141,12 @@ class NumpyCCA(object):
 
     def species_information(self):
         """
-        Return weights and effective number of species occurences
+        Return weights and effective number of species occurrences
         """
-        species_weights = np.sum(self.X, axis=0)
-        a = np.square(np.divide(self.X, species_weights))
+        species_weights = np.sum(self.x, axis=0)
+        a = np.square(np.divide(self.x, species_weights))
         species_n2 = 1.0 / a.sum(axis=0)
-        return (species_weights, species_n2)
+        return species_weights, species_n2
 
     def site_lc_scores(self):
         """
@@ -172,10 +167,10 @@ class NumpyCCA(object):
         """
         Return site weights and the effective number of species at a site
         """
-        site_weights = np.sum(self.X, axis=1)
-        a = np.square(np.divide(self.X, np.expand_dims(site_weights, axis=1)))
+        site_weights = np.sum(self.x, axis=1)
+        a = np.square(np.divide(self.x, np.expand_dims(site_weights, axis=1)))
         site_n2 = 1.0 / a.sum(axis=1)
-        return (site_weights, site_n2)
+        return site_weights, site_n2
 
 
 class NumpyRDA(object):
@@ -190,7 +185,7 @@ class NumpyRDA(object):
     when compared against vegan
     """
 
-    def __init__(self, X, Y):
+    def __init__(self, x, y):
         """
         Initialize the X (species) and Y (environmental variable) matrices
         and run the ordination.  Key variables are held as properties.
@@ -199,41 +194,33 @@ class NumpyRDA(object):
 
         Parameters
         ----------
-        X : numpy array
+        x : np.array
             Species matrix
-
-        Y : numpy array
+        y : np.array
             Environment matrix
-
-        Returns
-        -------
-        None
         """
-        self.X = X
-        self.Y = Y
+        self.x = x
+        self.y = y
 
-        ZERO = 0.0001
+        num_rows = x.shape[0] - 1
 
-        num_rows = X.shape[0] - 1
-
-        col_means = np.mean(X, axis=0).reshape(1, -1)
-        X_bar = self.X - col_means
-        sd = np.std(X_bar, axis=0)
-        s = np.linalg.svd(X_bar, full_matrices=False, compute_uv=False)
+        col_means = np.mean(x, axis=0).reshape(1, -1)
+        x_bar = self.x - col_means
+        _, s, _ = np.linalg.svd(x_bar, full_matrices=False)
         self.tot_chi = np.sum(s * s) / num_rows
 
-        col_means = np.mean(Y, axis=0).reshape(1, -1)
-        self.Y_r = self.Y - col_means
+        col_means = np.mean(y, axis=0).reshape(1, -1)
+        self.y_r = self.y - col_means
         self.env_means = col_means.flat
 
-        # Perform QR decompostion on the weighted Y matrix
-        (self.Q, self.R) = np.linalg.qr(self.Y_r, mode='full')
+        # Perform QR decomposition on the weighted Y matrix
+        self.q, self.r = np.linalg.qr(self.y_r, mode='full')
 
         # Run MLR fitting for Y matrix
-        right = np.dot(self.Q.T, X_bar)
-        (ls, res, rank, s) = np.linalg.lstsq(self.R, right)
-        Y = np.dot(self.Y_r, ls)
-        (u, s, v) = np.linalg.svd(Y, full_matrices=False)
+        right = np.dot(self.q.T, x_bar)
+        ls, res, rank, s = np.linalg.lstsq(self.r, right)
+        y = np.dot(self.y_r, ls)
+        u, s, v = np.linalg.svd(y, full_matrices=False)
 
         # Transform v for later calculations
         v = v.T
@@ -256,7 +243,7 @@ class NumpyRDA(object):
         self.u_eig = np.multiply(self.u, s[0:rank])
         self.v_eig = np.multiply(self.v, s[0:rank])
 
-        a = np.dot(X_bar, v[:, 0:rank])
+        a = np.dot(x_bar, v[:, 0:rank])
         self.wa_eig = a / math.sqrt(num_rows)
 
         self.wa = np.multiply(self.wa_eig, (1.0 / s[0:rank]))
@@ -266,15 +253,15 @@ class NumpyRDA(object):
         Return biplot scores of environmental variable by axis
         """
         biplot_scores = np.corrcoef(
-            self.Y_r, self.u[:, 0:self.rank], rowvar=0)
+            self.y_r, self.u[:, 0:self.rank], rowvar=False)
         return biplot_scores[0:self.rank, self.rank:]
 
     def coefficients(self):
         """
         Return the environmental variable loadings of for each axis
         """
-        right = np.dot(self.Q.T, self.u[:, 0:self.rank])
-        (x, resid, rank, s) = np.linalg.lstsq(self.R, right)
+        right = np.dot(self.q.T, self.u[:, 0:self.rank])
+        (x, residual, rank, s) = np.linalg.lstsq(self.r, right)
         return np.array(x)
 
     def species_centroids(self):
@@ -298,7 +285,7 @@ class NumpyRDA(object):
             (uk.shape[0], xi.shape[0], xi.shape[1]), dtype=np.float64)
         for (i, s) in enumerate(uk):
             xiuk[i] = xi - s
-        y = self.X.T
+        y = self.x.T
         y_xiuk_sqr = np.zeros((uk.shape[0], uk.shape[1]), dtype=np.float64)
         for i in range(y.shape[0]):
             y_xiuk_sqr[i] = np.dot(y[i], np.square(xiuk[i]))
@@ -307,12 +294,12 @@ class NumpyRDA(object):
 
     def species_information(self):
         """
-        Return weights and effective number of species occurences
+        Return weights and effective number of species occurrences
         """
-        species_weights = np.sum(self.X, axis=0)
-        a = np.square(np.divide(self.X, species_weights))
+        species_weights = np.sum(self.x, axis=0)
+        a = np.square(np.divide(self.x, species_weights))
         species_n2 = 1.0 / a.sum(axis=0)
-        return (species_weights, species_n2)
+        return species_weights, species_n2
 
     def site_lc_scores(self):
         """
@@ -333,7 +320,7 @@ class NumpyRDA(object):
         """
         Return site weights and the effective number of species at a site
         """
-        site_weights = np.sum(self.X, axis=1)
-        a = np.square(np.divide(self.X, np.expand_dims(site_weights, axis=1)))
+        site_weights = np.sum(self.x, axis=1)
+        a = np.square(np.divide(self.x, np.expand_dims(site_weights, axis=1)))
         site_n2 = 1.0 / a.sum(axis=1)
-        return (site_weights, site_n2)
+        return site_weights, site_n2
