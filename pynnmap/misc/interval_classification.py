f6089844ba54b26354868c837f334533917fdd6a
Matt Gregory <matt.gregory@oregonstate.edu>
2020-05-20 09:53:29 -0700

    Reorganization of interval_classification and quantile logic

--- pynnmap/misc/interval_classification.py
+++ pynnmap/misc/interval_classification.py
@@ -1,121 +1,113 @@
 import numpy as np
+from pynnmap.misc.weighted_array import WeightedArray
 
 
-class VariableVW(object):
+def get_global_range(*datasets):
     """
-    Class to store the values and weights of any variable of interest.
+    Given one or more datasets (either arrays or WeightedArray instances,
+    return the global min and max from all datasets
+
+    Parameters
+    ----------
+    datasets : sequence of lists, nd-arrays, WeightedArray instances
+        The set of datasets to use in calculating global min and max
+
+    Returns
+    -------
+    min : float
+        The global min across datasets
+    max : float
+        The global max across datasets
+    """
+    if len(list(datasets)) == 0:
+        msg = "One of more datasets is needed"
+        raise ValueError(msg)
+    min_list = []
+    max_list = []
+    for d in datasets:
+        if isinstance(d, WeightedArray):
+            d = d.values
+        d = np.asanyarray(d)
+        min_list.append(d.min())
+        max_list.append(d.max())
+    return np.array(min_list).min(), np.array(max_list).max()
+
+
+# class IntervalClassifier:
+#     """
+#     Base class for all interval classifiers
+#     """
+#     def __init__(self):
+#         self.edges = np.array([])
+#
+#     def __repr__(self):
+#         out_str = ''
+#         e = self.edges
+#         for i in range(e.size - 1):
+#             out_str += '({:.4f} - {:.4f})\n'.format(e[i], e[i+1])
+#         return out_str
+
+
+class DynamicClassifier:
+    """
+    Base class for classifiers where endpoints are to be determined.  Only
+    the number of bins is specified
     """
 
-    def __init__(self, values, weights):
-        """
-        Constructor for VariableVW
+    def __init__(self, bin_count):
+        try:
+            bin_count = int(bin_count)
+        except ValueError:
+            raise ValueError("Bins must be a number")
 
-        Parameters
-        ----------
-        values : np.array
-            Array of values
-
-        weights : np.array
-            Array of associated weights, same size as values
-
-        Returns
-        -------
-        None
-        """
-
-        self.values = values
-        self.weights = weights
+        if bin_count <= 0:
+            raise ValueError("Number of bins must be a positive integer")
+        self.bin_count = bin_count
 
 
-class IntervalClassifier(object):
-    """
-    Base class for all interval classifiers
-    """
-
-    def __init__(self):
-        self.edges = np.array([])
-
-    def __repr__(self):
-        out_str = ''
-        for i in range(self.edges.size - 1):
-            out_str += \
-                '(%.4f' % self.edges[i] + ' - ' + '%.4f)\n' % self.edges[i + 1]
-        return out_str
-
-
-class EqualIntervalClassifier(IntervalClassifier):
+class EqualIntervalClassifier(DynamicClassifier):
     """
     Classifier to find class endpoints based on equal interval splits
     of the range of all datasets
     """
 
-    def __init__(self, datasets, bins=10):
-        """
-        Set the edges of the classification given one or more datasets and
-        the given number of bins.
+    def __init__(self, bin_count):
+        super().__init__(bin_count)
 
-        The algorithm first determines the range of the classifier based on
-        the range present in all the datasets.
-
-        Parameters
-        ----------
-        datasets : list of array-like objects
-
-        bins : int
-            Number of bins
-
-        Returns
-        -------
-        edges : np.array
-            The edges of the classifier.  This list will be one larger than
-            the number of bins
-        """
-
-        super(EqualIntervalClassifier, self).__init__()
-
-        # Find the absolute min and max of all datasets.  Assume we have
-        # VariableVW instances and catch any array-like objects
-
-        try:
-            abs_min = min(datasets[0].values)
-            abs_max = max(datasets[0].values)
-        except AttributeError:
-            abs_min = min(datasets[0])
-            abs_max = max(datasets[0])
-
-        for ds in datasets:
-            try:
-                if min(ds.values) < abs_min:
-                    abs_min = min(ds.values)
-                if max(ds.values) > abs_max:
-                    abs_max = max(ds.values)
-            except AttributeError:
-                if min(ds) < abs_min:
-                    abs_min = min(ds)
-                if max(ds) > abs_max:
-                    abs_max = max(ds)
-
-        # Make sure bins can be cast as an integer
-        try:
-            bins = int(bins)
-        except ValueError:
-            raise ValueError('Bins must be a number')
-
-        # Catch negative bins - otherwise they silently succeed
-        if bins <= 0:
-            raise ValueError('Number of bins must be a positive integer')
-
-        # Create the bins based on these values
-        self.edges = np.linspace(abs_min, abs_max, num=bins + 1)
+    def __call__(self, arr):
+        arr = np.asanyarray(arr)
+        return np.linspace(arr.min(), arr.max(), num=self.bin_count + 1)
 
 
-class QuantileClassifier(IntervalClassifier):
+def approx_quantiles(arr, bin_count):
+    if arr.size <= bin_count:
+        return np.sort(arr)
+    q = np.linspace(0, 1, bin_count + 1)
+    bins = np.quantile(arr, q)
+    uniq, counts = np.unique(bins, return_counts=True)
+    dup = uniq[counts > 1]
+    if len(dup):
+        new = arr[arr != dup[0]]
+        return np.sort(
+            np.hstack((dup[0], approx_quantiles(new, bin_count - 1)))
+        )
+    return bins
 
-    def __init__(self):
-        super(QuantileClassifier, self).__init__()
+
+class QuantileClassifier(DynamicClassifier):
+    """
+    Classifier to find class endpoints based on equal bin counts among
+    classes
+    """
+
+    def __init__(self, bin_count: int = 10):
+        super().__init__(bin_count)
+
+    def __call__(self, arr: np.ndarray):
+        return approx_quantiles(arr, self.bin_count)
 
 
-class CustomIntervalClassifier(IntervalClassifier):
+class CustomIntervalClassifier:
     """
     Classifier to set class endpoints based on user-supplied values
     """
@@ -128,19 +120,15 @@ class CustomIntervalClassifier(IntervalClassifier):
         ----------
         bins : 1-d list
             Bin endpoints
-
-        Returns
-        -------
-        edges : np.array
-            The edges of the classifier.
         """
-
-        super(CustomIntervalClassifier, self).__init__()
         bins = np.array(bins).ravel()
         if bins.size < 1:
-            raise ValueError('Bins must not be empty')
+            raise ValueError("Bins must not be empty")
         if bins.size == 1:
             bins = np.append(bins, bins[0])
         if (np.diff(bins) < 0).any():
-                raise AttributeError('Bins must increase monotonically')
-        self.edges = np.array(bins)
+            raise AttributeError("Bins must increase monotonically")
+        self.bins = bins
+
+    def __call__(self, arr):
+        return self.bins
