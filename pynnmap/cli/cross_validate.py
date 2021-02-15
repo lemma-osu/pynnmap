b1ddf0b73cc3bc8e7e3af5d5cd6b915fbfef5215
Matt Gregory <matt.gregory@oregonstate.edu>
2021-02-15 14:47:07 -0800

    Create function to run cross-validate to remove duplication

--- pynnmap/cli/cross_validate.py
+++ pynnmap/cli/cross_validate.py
@@ -6,22 +6,7 @@ from pynnmap.diagnostics import diagnostic_wrapper as dw
 from pynnmap.parser import parameter_parser_factory as ppf
 
 
-@click.command(short_help="Accuracy assessment for model plots")
-@click.option(
-    "--scale",
-    type=click.Choice(["PIXEL", "PLOT"], case_sensitive=False),
-    default="PIXEL",
-    help="Calculate accuracy at pixel or plot scale"
-)
-@click.argument("parameter-file", type=click.Path(exists=True), required=True)
-def cross_validate(scale, parameter_file):
-    # Get the model parameters
-    pixel_scale = scale == "PIXEL"
-    parser = ppf.get_parameter_parser(parameter_file)
-
-    # Create a NNFinder derived object - either pixel or plot
-    finder = PixelNNFinder(parser) if pixel_scale else PlotNNFinder(parser)
-
+def run_cross_validate(parser, finder):
     # Run cross-validation to create the neighbor/distance information
     neighbor_data = finder.calculate_neighbors_cross_validation()
 
@@ -34,6 +19,26 @@ def cross_validate(scale, parameter_file):
     output.write_zonal_records(parser.dependent_zonal_pixel_file)
     output.write_attribute_predictions(parser.dependent_predicted_file)
     output.write_nn_index_file(neighbor_data, parser.dependent_nn_index_file)
+
+
+@click.command(short_help="Accuracy assessment for model plots")
+@click.option(
+    "--scale",
+    type=click.Choice(["PIXEL", "PLOT"], case_sensitive=False),
+    default="PIXEL",
+    help="Calculate accuracy at pixel or plot scale",
+)
+@click.argument("parameter-file", type=click.Path(exists=True), required=True)
+def cross_validate(scale, parameter_file):
+    # Get the model parameters
+    pixel_scale = scale == "PIXEL"
+    parser = ppf.get_parameter_parser(parameter_file)
+
+    # Create a NNFinder derived object - either pixel or plot
+    finder = PixelNNFinder(parser) if pixel_scale else PlotNNFinder(parser)
+
+    # Run cross-validation to create the neighbor/distance information
+    run_cross_validate(parser, finder)
 
     # Calculate all accuracy diagnostics
     diagnostic_wrapper = dw.DiagnosticWrapper(parser)
