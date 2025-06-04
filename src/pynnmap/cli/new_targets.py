8461042b21630a5e1cadd8113726c220cb441f11
Matt Gregory <matt.gregory@oregonstate.edu>
2025-06-04 15:00:31 -0700

    Implement `new-targets` CLI entry point (#15)
    This PR implements the new_targets entry point. The script relies on
    the following inputs:
    
    - `parameter-file`: The standard parameter file (typically model.xml)
    - `target-plot-file`: A file with the new targets for which predicted
       attributes and neighbors should be collected. The script expects that
       this file will have a field that matches the value of
       `nn_parameters.file_locations.plot_id_field`.
    - `output-predicted-prefix`: The basename for where predicted
       output should be written. Both independent and dependent predicted
       files are written with `_independent.csv` and `_dependent.csv`
       respectively appended to this prefix.
    - `output-zonal-prefix`: The basename for where zonal output should
       be written. Both independent and dependent zonal files are written
       with `_pixel_independent.csv` and `_pixel_dependent.csv` respectively
       appended to this prefix.
    
    For this CLI program, we've gone away from specifying all parameters
    within the model configuration file which provides some flexibility in
    running new targets with more than one set of plots, such that output
    can be customized based on the output prefixes.

--- src/pynnmap/cli/new_targets.py
+++ src/pynnmap/cli/new_targets.py
@@ -1,9 +1,46 @@
+from __future__ import annotations
+
 import click
+import pandas as pd
+
+from ..core.nn_finder import PixelNNFinder
+from ..core.prediction_output import DependentOutput, IndependentOutput
+from ..parser import parameter_parser_factory as ppf
 
 
 @click.command(
     name="new-targets", short_help="Run accuracy assessment on independent plots"
 )
 @click.argument("parameter-file", type=click.Path(exists=True), required=True)
-def main():
-    print("Not Implemented")
+@click.argument("target-plot-file", type=click.Path(exists=True), required=True)
+@click.argument("output-predicted-prefix", type=click.STRING, required=True)
+@click.argument("output-zonal-prefix", type=click.STRING, required=True)
+def main(
+    parameter_file,
+    target_plot_file,
+    output_predicted_prefix,
+    output_zonal_prefix,
+):
+    # Get the model parameters
+    parser = ppf.get_parameter_parser(parameter_file)
+
+    # Read in the target IDs
+    id_field = parser.plot_id_field
+    target_ids = pd.read_csv(target_plot_file, usecols=[id_field])[id_field].tolist()
+
+    # Calculate neighbors for the new target plots
+    finder = PixelNNFinder(parser)
+    neighbor_data = finder.calculate_neighbors_at_ids(target_ids)
+
+    # Calculate independent and dependent predictive accuracy
+    independent_output = IndependentOutput(parser, neighbor_data)
+    independent_zonal_pixel_file = f"{output_zonal_prefix}_pixel_independent.csv"
+    independent_predicted_file = f"{output_predicted_prefix}_independent.csv"
+    independent_output.write_zonal_records(independent_zonal_pixel_file)
+    independent_output.write_attribute_predictions(independent_predicted_file)
+
+    dependent_output = DependentOutput(parser, neighbor_data)
+    dependent_zonal_pixel_file = f"{output_zonal_prefix}_pixel_dependent.csv"
+    dependent_predicted_file = f"{output_predicted_prefix}_dependent.csv"
+    dependent_output.write_zonal_records(dependent_zonal_pixel_file)
+    dependent_output.write_attribute_predictions(dependent_predicted_file)
