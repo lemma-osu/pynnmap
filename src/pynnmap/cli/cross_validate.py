74a31f1ed31d57c51655a74cf36160769fbf1248
Matt Gregory <matt.gregory@oregonstate.edu>
2025-07-11 12:02:13 -0700

    Calculate cross-validation from stored pixel-level files
    Historically, for cross validation, plot ID values used in modeling are
    captured and then pixel-level values at plot footprints are extracted
    based on covariates in the model configuration.  This can be a long
    process although it remains necessary when obtaining new targets for
    which the user has no previously calculated covariate information.
    
    However, for cross validation, a user can create a CSV file that stores
    pixel values for all covariates for all model plots and use this
    information instead.  This PR creates a new `NNFinder` derived class
    (`StoredPixelNNFinder`) that simply reads in the CSV file and creates
    covariate information for the model plots.  The cross-validate CLI
    program looks for the presence of the `environmental_pixel_file` tag in
    the configuration and, if present, uses this class, otherwise defaults
    to the `PixelNNFinder` which does the extractions on the fly.

--- src/pynnmap/cli/cross_validate.py
+++ src/pynnmap/cli/cross_validate.py
@@ -2,25 +2,10 @@ from __future__ import annotations
 
 import click
 
-from ..core.nn_finder import PixelNNFinder, PlotNNFinder
+from ..core.nn_finder import PixelNNFinder, PlotNNFinder, StoredPixelNNFinder
 from ..core.prediction_output import DependentOutput, IndependentOutput
 from ..diagnostics import diagnostic_wrapper as dw
 from ..parser import parameter_parser_factory as ppf
-
-
-def run_cross_validate(parser, finder: PixelNNFinder | PlotNNFinder) -> None:
-    # Run cross-validation to create the neighbor/distance information
-    neighbor_data = finder.calculate_neighbors_cross_validation()
-
-    # Calculate independent and dependent predictive accuracy
-    independent_output = IndependentOutput(parser, neighbor_data)
-    independent_output.write_zonal_records(parser.independent_zonal_pixel_file)
-    independent_output.write_attribute_predictions(parser.independent_predicted_file)
-
-    dependent_output = DependentOutput(parser, neighbor_data)
-    dependent_output.write_zonal_records(parser.dependent_zonal_pixel_file)
-    dependent_output.write_attribute_predictions(parser.dependent_predicted_file)
-    dependent_output.write_nn_index_file(neighbor_data, parser.dependent_nn_index_file)
 
 
 @click.command(name="cross-validate", short_help="Accuracy assessment for model plots")
@@ -36,17 +21,29 @@ def main(scale, parameter_file):
     pixel_scale = scale == "PIXEL"
     parser = ppf.get_parameter_parser(parameter_file)
 
-    # Traditional route - extracting spatial information from plot locations
-    if not parser.environmental_pixel_file:
-        # Create a NNFinder derived object - either pixel or plot
-        finder = PixelNNFinder(parser) if pixel_scale else PlotNNFinder(parser)
-
-        # Run cross-validation to create the neighbor/distance information
-        run_cross_validate(parser, finder)
-
-    # Fast route - using pre-stored spatial information at pixel locations
+    # Determine the NNFinder type
+    if pixel_scale:
+        # Traditional route - extracting spatial information from plot locations
+        if not parser.environmental_pixel_file:
+            nn_finder = PixelNNFinder(parser)
+        # Fast route - using pre-stored spatial information at pixel locations
+        else:
+            nn_finder = StoredPixelNNFinder(parser)
     else:
-        pass
+        nn_finder = PlotNNFinder(parser)
+
+    # Run cross-validation to create the neighbor/distance information
+    neighbor_data = nn_finder.calculate_neighbors_cross_validation()
+
+    # Calculate independent and dependent predictive accuracy
+    independent_output = IndependentOutput(parser, neighbor_data)
+    independent_output.write_zonal_records(parser.independent_zonal_pixel_file)
+    independent_output.write_attribute_predictions(parser.independent_predicted_file)
+
+    dependent_output = DependentOutput(parser, neighbor_data)
+    dependent_output.write_zonal_records(parser.dependent_zonal_pixel_file)
+    dependent_output.write_attribute_predictions(parser.dependent_predicted_file)
+    dependent_output.write_nn_index_file(neighbor_data, parser.dependent_nn_index_file)
 
     # Calculate all accuracy diagnostics
     diagnostic_wrapper = dw.DiagnosticWrapper(parser)
